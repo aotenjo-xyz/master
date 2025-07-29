6dd696a36a0001cf939e9dc52a24e641988aead3
Yuichiro Aoki <45054071+yuichiroaoki@users.noreply.github.com>
2025-07-29 22:31:46 +0900

    M4 (#2)
    * refactor command parsing logic for motor control
    
    * refactor CAN message handling to use offsets for motor IDs
    
    * add handling for motor position command in parseCommand function

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ class RxFromCAN : public PingPongNotificationsFromCAN {
 public:
   RxFromCAN(){};
 
-  void ReceivedMotorPosition(const uint8_t *data, const MOTOR_ID motor_id) {
+  void ReceivedMotorPosition(const uint8_t *data, const uint32_t motor_id) {
     float angle = unpackAngleFromCanMessage(data);
     //   print M<id>P<angle>
     Serial.printf("M%dP%.2f\n", motor_id, angle);
@@ -42,76 +42,43 @@ void setup() {
   Serial.println("Start!");
 }
 
-void handleMotorAngleCommand(int _motor_id, CAN_ID _can_id, float targetAngle,
+void handleMotorAngleCommand(int _motor_id, uint32_t _can_id, float targetAngle,
                              CAN_msg_t *message) {
   Serial.printf("M%dA%.2f\n", _motor_id, targetAngle);
   message->id = _can_id;
   packAngleIntoCanMessage(message, targetAngle);
 }
 
-void handleMotorPositionCommand(int _motor_id, CAN_ID _can_id,
+void handleMotorPositionCommand(int _motor_id, uint32_t _can_id,
                                 CAN_msg_t *message) {
   Serial.printf(F("Requesting motor %d position\n"), _motor_id);
   message->id = _can_id;
 }
 
 PARSE_RESULT parseCommand(String command, CAN_msg_t *message) {
-  PARSE_RESULT result = ANG_CONTROL;
-  // Example command: M1A6.28
-  if (command.startsWith("M") && command.indexOf("A") != -1) {
-    int motorId = command.substring(1, command.indexOf("A")).toInt();
-    float targetAngle = command.substring(command.indexOf("A") + 1).toFloat();
+  if (command.startsWith("M")) {
+    int sepIdx = command.indexOf("A");
+    int posIdx = command.indexOf("P");
+    int motorId = -1;
 
-    switch (motorId) {
-    case MOTOR_0_ID:
-      handleMotorAngleCommand(motorId, M0_ANGLE_CNTL, targetAngle, message);
-      break;
-
-    case MOTOR_1_ID:
-      handleMotorAngleCommand(motorId, M1_ANGLE_CNTL, targetAngle, message);
-      break;
-
-    case MOTOR_2_ID:
-      handleMotorAngleCommand(motorId, M2_ANGLE_CNTL, targetAngle, message);
-      break;
-
-    case MOTOR_3_ID:
-      handleMotorAngleCommand(motorId, M3_ANGLE_CNTL, targetAngle, message);
-      break;
-
-    default:
-      result = INVALID_COMMAND;
-      break;
-    }
-
-  } else if (command.startsWith("M") && command.indexOf("P") != -1) {
-    // M0P: send a remote frame to get the motor0 position
-    int motorId = command.substring(1, command.indexOf("P")).toInt();
-    result = REQ_DATA;
-    switch (motorId) {
-    case MOTOR_0_ID:
-      handleMotorPositionCommand(motorId, M0_POS, message);
-      break;
-    case MOTOR_1_ID:
-      handleMotorPositionCommand(motorId, M1_POS, message);
-      break;
-    case MOTOR_2_ID:
-      handleMotorPositionCommand(motorId, M2_POS, message);
-      break;
-    case MOTOR_3_ID:
-      handleMotorPositionCommand(motorId, M3_POS, message);
-      break;
-    default:
-      result = INVALID_COMMAND;
-      break;
+    if (sepIdx != -1) { // Angle control: MxAangle
+      motorId = command.substring(1, sepIdx).toInt();
+      float targetAngle = command.substring(sepIdx + 1).toFloat();
+      handleMotorAngleCommand(motorId, motorId + ANGLE_COMMAND_OFFSET,
+                              targetAngle, message);
+      return ANG_CONTROL;
+    } else if (posIdx != -1) { // Position request: MxP
+      motorId = command.substring(1, posIdx).toInt();
+      handleMotorPositionCommand(motorId, motorId + POS_COMMAND_OFFSET,
+                                 message);
+      return REQ_DATA;
     }
   } else if (command.startsWith("ESTOP")) {
-    // Emergency stop command
     message->id = ESTOP;
-    result = EMERGENCY_STOP;
     Serial.println("Emergency stop command");
+    return EMERGENCY_STOP;
   }
-  return result;
+  return INVALID_COMMAND;
 }
 
 const int BUFFER_SIZE = 50;
