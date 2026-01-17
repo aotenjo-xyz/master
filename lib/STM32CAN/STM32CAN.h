9cff46ee89cc64015056bbb5e9386bb6dc029ff1
Yuichiro Aoki <yuichiroaoki27@gmail.com>
2026-01-18 03:10:49 +0900

    Add FDCAN data length conversion functions and update header

--- lib/STM32CAN/STM32CAN.h
+++ lib/STM32CAN/STM32CAN.h
@@ -14,3 +14,5 @@ typedef enum {
 
 void packAngleIntoCanMessage(uint8_t *message, float angle);
 float unpackAngleFromCanMessage(const uint8_t *data);
+uint32_t GetFDCANDataLengthCode(uint8_t bytes);
+uint8_t GetBytesFromFDCANDataLength(uint32_t dlc);
